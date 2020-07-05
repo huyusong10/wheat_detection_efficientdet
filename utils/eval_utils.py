precision bug fixed

utils/eval_utils.py
@@ -11,7 +11,7 @@ from typing import List, Union, Tuple
 
 
 @jit(nopython=True)
-def calculate_iou(gt, pr, form='pascal_voc') -> float:
+def calculate_iou(gt, pr) -> float:
     """Calculates the Intersection over Union.
 
     Args:
@@ -23,15 +23,6 @@ def calculate_iou(gt, pr, form='pascal_voc') -> float:
     Returns:
         (float) Intersection over union (0.0 <= iou <= 1.0)
     """
-    if form == 'coco':
-        gt = gt.copy()
-        pr = pr.copy()
-
-        gt[2] = gt[0] + gt[2]
-        gt[3] = gt[1] + gt[3]
-        pr[2] = pr[0] + pr[2]
-        pr[3] = pr[1] + pr[3]
-
     # Calculate overlap area
     dx = min(gt[2], pr[2]) - max(gt[0], pr[0]) + 1
 
@@ -52,7 +43,7 @@ def calculate_iou(gt, pr, form='pascal_voc') -> float:
     return overlap_area / union_area
 
 @jit(nopython=True)
-def find_best_match(gts, pred, pred_idx, threshold = 0.5, form = 'pascal_voc', ious=None) -> int:
+def find_best_match(gts, pred, pred_idx, threshold = 0.5, ious=None) -> int:
     """Returns the index of the 'best match' between the
     ground-truth boxes and the prediction. The 'best match'
     is the highest IoU. (0.0 IoUs are ignored).
@@ -80,7 +71,7 @@ def find_best_match(gts, pred, pred_idx, threshold = 0.5, form = 'pascal_voc', i
         iou = -1 if ious is None else ious[gt_idx][pred_idx]
 
         if iou < 0:
-            iou = calculate_iou(gts[gt_idx], pred, form=form)
+            iou = calculate_iou(gts[gt_idx], pred)
             
             if ious is not None:
                 ious[gt_idx][pred_idx] = iou
@@ -95,7 +86,7 @@ def find_best_match(gts, pred, pred_idx, threshold = 0.5, form = 'pascal_voc', i
     return best_match_idx
 
 @jit(nopython=True)
-def calculate_precision(gts, preds, threshold = 0.5, form = 'pascal_voc', ious=None) -> float:
+def calculate_precision(gts, preds, threshold, ious) -> float:
     """Calculates precision for GT - prediction pairs at one threshold.
 
     Args:
@@ -117,7 +108,7 @@ def calculate_precision(gts, preds, threshold = 0.5, form = 'pascal_voc', ious=N
     for pred_idx in range(n):
 
         best_match_gt_idx = find_best_match(gts, preds[pred_idx], pred_idx,
-                                            threshold=threshold, form=form, ious=ious)
+                                            threshold=threshold, ious=ious)
 
         if best_match_gt_idx >= 0:
             # True positive: The predicted box matches a gt box with an IoU above the threshold.
@@ -132,12 +123,11 @@ def calculate_precision(gts, preds, threshold = 0.5, form = 'pascal_voc', ious=N
 
     # False negative: indicates a gt box had no associated predicted box.
     fn = (gts.sum(axis=1) > 0).sum()
-
     return tp / (tp + fp + fn)
 
 
 @jit(nopython=True)
-def calculate_image_precision(gts, preds, thresholds = (0.5, 0.55, 0.6, 0.65, 0.7, 0.75), form = 'pascal_voc') -> float:
+def calculate_image_precision(gts, preds, thresholds = (0.5, 0.55, 0.6, 0.65, 0.7, 0.75)) -> float:
     """Calculates image precision.
 
     Args:
@@ -158,7 +148,7 @@ def calculate_image_precision(gts, preds, thresholds = (0.5, 0.55, 0.6, 0.65, 0.
 
     for threshold in thresholds:
         precision_at_threshold = calculate_precision(gts.copy(), preds, threshold=threshold,
-                                                     form=form, ious=ious)
+                                                     ious=ious)
         image_precision += precision_at_threshold / n_threshold
 
     return image_precision

wheat_detection_visualized.py
@@ -9,13 +9,13 @@ import numpy as np
 from efficientdet.utils import BBoxTransform, ClipBoxes
 from utils.utils import preprocess, invert_affine, postprocess
 
-compound_coef = 0
-img_path = '/Volumes/hys portable/Users/hys portable/Downloads portable/global-wheat-detection/test/53f253011.jpg'
-pth_path = '/Users/hys/Downloads/final_stage.pth'
+compound_coef = 4
+img_path = '/home/huys/wheat_detection/test/2fd875eaa.jpg'
+pth_path = '/home/huys/wheat_detection/result/model_d4_3e-4_1200_0.7/savedByLoss-d4_2_2025.pth'
 
 threshold = 0.5
 iou_threshold = 0.25
-input_size = 512
+input_size = 1024
 
 
 obj_list = ['wheat spike']
@@ -42,7 +42,7 @@ with torch.no_grad():
     out = postprocess(x,
                       anchors, regression, classification,
                       regressBoxes, clipBoxes,
-                      use_WBF=True, WBF_thr=0.5, WBF_iou_thr=0.55, input_size=512)
+                      use_WBF=False, WBF_thr=threshold, WBF_iou_thr=iou_threshold, input_size=1024)
 
 out = invert_affine(framed_metas, out)
 

wheat_eval.py
@@ -12,27 +12,27 @@ from utils.utils import postprocess
 from utils.eval_utils import calculate_image_precision, calculate_precision
 from wheat_data import get_data_set, collate_fn
 
-torch.cuda.set_device(3)
-use_cuda = False
-compound_coef = 0
-pth_path = '/home/huys/wheat_detection/result/model_without_precision_2/savedByLoss-d0_5_1260.pth'
+torch.cuda.set_device(0)
+use_cuda = True
+compound_coef = 4
+pth_path = '/home/huys/wheat_detection/result/model_d4_1e-3_1200_0.5/savedByLoss-d4_3_2700.pth'
 # pth_path = '/home/huys/wheat_detection/result/model_test/final_stage.pth'
 
 threshold = 0.5
-iou_threshold = 0.2
+iou_threshold = 0.25
 obj_list = ['wheat spike']
-batch_size = 16
+batch_size = 4
 
 val_params = {'batch_size': batch_size,
             'shuffle': False,
             'drop_last': True,
             'collate_fn': collate_fn,
-            'num_workers': 16}
+            'num_workers': 4}
 
-def eval_data(dataset, dataset_params, model, threshold=0.5, iou_threshold=0.2):
+def eval_data(dataset, dataset_params, model, threshold, iou_threshold):
     val_generator = DataLoader(dataset, **dataset_params)
     eval_result = []
-    for data in tqdm(val_generator):
+    for i, data in enumerate(tqdm(val_generator)):
         if use_cuda:
             imgs = torch.stack([img.cuda() for img in data['img']], 0)
         else:
@@ -59,12 +59,14 @@ def eval_data(dataset, dataset_params, model, threshold=0.5, iou_threshold=0.2):
         batch_result = []
         for i in range(batch_size):
             preds = out[i]['rois'].astype(int)
+            if preds.size == 0:
+                batch_result.append(0)
+                continue
             gts = batch_gts[i]
             gts = gts[gts[::,4] > -1].numpy()
-            image_precision = calculate_image_precision(preds,
-                                                        gts,
-                                                        thresholds=(0.5, 0.55, 0.6, 0.65, 0.7, 0.75),
-                                                        form='pascal_voc')
+            image_precision = calculate_image_precision(gts,
+                                                        preds,
+                                                        thresholds=(0.5, 0.55, 0.6, 0.65, 0.7, 0.75),)
             batch_result.append(image_precision)
 
         mean_precision = np.mean(batch_result)
@@ -91,7 +93,7 @@ if __name__ == '__main__':
     if use_cuda:
         model.cuda()
 
-    result = eval_data(val_set, val_params, model)
+    result = eval_data(val_set, val_params, model, threshold, iou_threshold)
     print(result)
 
 

wheat_train.py
@@ -177,7 +177,7 @@ def train(params):
 
     writer = SummaryWriter(
         params.log_path +
-        f'/{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}/')
+        f'/{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}'+f'_d{params.compound_coef}_{params.lr}/')
 
     model_with_loss = ModelWithLoss(model, debug=params.debug)
 
@@ -198,7 +198,7 @@ def train(params):
                                     momentum=0.9,
                                     nesterov=True)
     else:
-        print(f'无{params.lr}优化器')
+        print(f'无{params.optim}优化器')
         raise Exception
 
     # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
@@ -207,7 +207,7 @@ def train(params):
     #                                                        cooldown=50,
     #                                                        min_lr=1e-6)
     # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 50, 3e-6)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 600, gamma=0.1)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1200, gamma=0.5)
 
     epoch = 0
     best_loss = 1e5
@@ -322,9 +322,6 @@ def train(params):
                                 continue
                         except Exception as e:
                             print(e)
-                            print('cls_loss: ', cls_loss)
-                            print('reg_loss: ', reg_loss)
-
                         if use_precision:
                             try:
                                 features, regression, classification, anchors = model(imgs)
@@ -335,17 +332,17 @@ def train(params):
                                                 anchors, regression, classification,
                                                 regressBoxes, clipBoxes,
                                                 params.threshold, params.iou_threshold)
-                                if len(out[i]['rois']) == 0:
-                                    print(out)
                                 batch_precision = []
                                 for i in range(params.batch_size):
                                     preds = out[i]['rois'].astype(int)
+                                    if preds.size == 0:
+                                        batch_precision.append(0)
+                                        continue
                                     gts = batch_gts[i]
                                     gts = gts[gts[::,4] > -1].numpy()
-                                    image_precision = calculate_image_precision(preds,
-                                                                                gts,
-                                                                                thresholds=eval(params.eval_thresholds),
-                                                                                form='pascal_voc')
+                                    image_precision = calculate_image_precision(gts,
+                                                                                preds,
+                                                                                thresholds=eval(params.eval_thresholds),)
                                     batch_precision.append(image_precision)
                                 mean_precision = np.mean(batch_precision)
                                 precision_ls.append(mean_precision)

which_is_best.py
@@ -94,18 +94,18 @@ if __name__ == '__main__':
                 scores = torch.max(classification, dim=2, keepdim=True)[0]
                 scores_over_thresh = (scores > threshold)[:, :, 0]
                 out = postprocess_on_iou_threshold(batch_size, classification, anchors, scores, scores_over_thresh, iou_threshold)
-                if len(out) == 0:
-                    print('out空')
                 batch_result = []
                 for x in range(len(out)):
                     preds = out[x]['rois'].astype(int)
+                    if preds.size == 0:
+                        batch_result.append(0)
+                        continue
                     gts = gts_on_batch[x]
                     gts = gts[gts[::,4] > -1].numpy()
 
-                    image_precision = calculate_image_precision(preds,
-                                                                gts,
-                                                                thresholds=(0.5, 0.55, 0.6, 0.65, 0.7, 0.75),
-                                                                form='pascal_voc')
+                    image_precision = calculate_image_precision(gts,
+                                                                preds,
+                                                                thresholds=(0.5, 0.55, 0.6, 0.65, 0.7, 0.75),)
                     batch_result.append(image_precision)
                 mean_precision = np.mean(batch_result)
                 eval_result.append(mean_precision)